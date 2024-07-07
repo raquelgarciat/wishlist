b093b8a4ea15549d8ee4a7453036933450354941
glintingblur <grimoirerosae@gmail.com>
2024-07-07 17:39:40 +0200

    Publish in GitHub Pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,54 +1,48 @@
-// IMPORTS
-
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { createClient } from '@supabase/supabase-js';
 import { Routes, Route, Link } from 'react-router-dom';
 import { useLocation, matchPath } from 'react-router';
-
-import getDataFromApi from '../services/api';
 
 import '../styles/main.scss';
 
 import logo from '../images/logo.png';
 
 import CharacterList from './CharacterList';
-import Filters from './Filters';
 import CharacterDetail from './CharacterDetail';
 
-// FUNCIÓN DEL COMPONENTE
+const supabase = createClient(
+  'https://rtcnwwlfcwswsnhcuzod.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ0Y253d2xmY3dzd3NuaGN1em9kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjAyODgxNjYsImV4cCI6MjAzNTg2NDE2Nn0.d1Src9uf7rpbDLkeHaDxIanU_C9SF7dGJ9TapghtQ2E'
+);
 
 function App() {
   // SECCIÓN DE VARIABLES DE ESTADO
   const [characterList, setCharacterList] = useState([]);
-  const [searchByName, setSearchByName] = useState('');
-  const [searchBySpecies, setSearchBySpecies] = useState('');
+  const [products, setProducts] = useState([]);
 
-  // SECCIÓN DE CÓDIGO CUANDO CARGA LA PÁGINA
   useEffect(() => {
-    getDataFromApi().then((cleanData) => {
-      setCharacterList(cleanData);
-    });
+    getData();
   }, []);
 
-  // SECCIÓN DE HANDLERS/EVENTOS
-  const handleFilter = (varName, varValue) => {
-    if (varName === 'name') {
-      setSearchByName(varValue);
-    } else if (varName === 'species') {
-      setSearchBySpecies(varValue);
-    }
+  async function getData() {
+    const { data } = await supabase.from('products').select();
+    setCharacterList(data);
+  }
+
+  const getDataFromApi = () => {
+    const cleanData = products.map((objectAPI) => {
+      return {
+        id: objectAPI.id,
+        name: objectAPI.name,
+        image: objectAPI.image,
+        status: objectAPI.status,
+        species: objectAPI.species,
+      };
+    });
+    return cleanData;
   };
 
-  // SECCIÓN FUNCIONES DE PINTADO
-  const filteredCharacters = characterList
-    .filter((eachCharacter) =>
-      eachCharacter.name.toLowerCase().includes(searchByName.toLowerCase())
-    )
-    .filter((eachCharacter) =>
-      eachCharacter.species
-        .toLowerCase()
-        .includes(searchBySpecies.toLowerCase())
-    );
-  const species = characterList.map((eachCharacter) => eachCharacter.species);
+  getDataFromApi();
 
   // OBTENER LA INFORMACIÓN DEL CONTACTO
   const { pathname } = useLocation();
@@ -73,14 +67,8 @@ function App() {
             path='/'
             element={
               <>
-                <Filters
-                  searchByName={searchByName}
-                  searchBySpecies={searchBySpecies}
-                  handleFilter={handleFilter}
-                  species={species}
-                />
                 <section className='charlist'>
-                  <CharacterList characterList={filteredCharacters} />
+                  <CharacterList characterList={characterList} />
                 </section>
               </>
             }
@@ -93,13 +81,14 @@ function App() {
             path='*'
             element={
               <div>
-              <h2 className='errormsg'>
-                <i className="fa-solid fa-skull-crossbones"></i> Error 404: page not
-                found
-              </h2>
-              <Link className='detail_back' to='/'>
-              <i className='fa-solid fa-circle-left'></i> Go back
-            </Link></div>
+                <h2 className='errormsg'>
+                  <i className='fa-solid fa-skull-crossbones'></i> Error 404:
+                  page not found
+                </h2>
+                <Link className='detail_back' to='/'>
+                  <i className='fa-solid fa-circle-left'></i> Go back
+                </Link>
+              </div>
             }
           />
         </Routes>
@@ -107,7 +96,5 @@ function App() {
     </div>
   );
 }
-
-// EXPORT DEL COMPONENTE
 
 export default App;
