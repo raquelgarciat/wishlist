f12fbcc8bd91ae36f9645e81eb4bb2030992ba96
glintingblur <grimoirerosae@gmail.com>
2024-07-13 13:11:13 +0200

    Publish in GitHub Pages

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -3,50 +3,45 @@ import { Link } from 'react-router-dom';
 function CharacterDetail({ characterData }) {
   if (characterData) {
     return (
-      <div className='detail'>
-        <section className='card'>
+      <div className="detail">
+        <section className="card">
           <img
-            className='card_image'
+            className="card_image"
             src={characterData.image}
             alt={`${characterData.name}`}
           />
           <div>
-            <h2 className='card_title'>{characterData.name}</h2>
-            <ul className='card_data'>
-              <li className='card_data-li'>
-                <span className='datatitle'>Description: </span>
-                {characterData.itemdesc}
+            <h2 className="card_title">{characterData.name}</h2>
+            <ul className="card_data">
+              <li className="card_data-li">
+                <p className="datatitle">DESCRIPTION</p>
+                <p>{characterData.itemdesc}</p>
               </li>
-              <li className='card_data-li'>
-                <span className='datatitle'>Category: </span>
-                {characterData.category}
+              <li className="card_data-li">
+                <p className="datatitle">CATEGORIES </p>
+                <p>{characterData.category}</p>
               </li>
-              <li className='card_data-li'>
-                <span className='datatitle'>Shop: </span>
-                {characterData.shop}
-              </li>
-              <li className='card_data-li'>
-                <span className='datatitle'>
-                  {' '}
-                  <a href={characterData.producturl}>Buy here!</a>
-                </span>
+
+              <li className="card_data-li">
+                <p className="datatitle">WHERE TO BUY</p>
+                <a href={characterData.producturl}>{characterData.shop}</a>
               </li>
             </ul>
           </div>
         </section>
-        <Link className='detail_back' to='/'>
-          <i className='fa-solid fa-circle-left'></i> Go back
+        <Link className="detail_back" to="/">
+          <i className="fa-solid fa-circle-left"></i> Go back
         </Link>
       </div>
     );
   } else {
     return (
       <div>
-        <p className='detail_errormsg'>
+        <p className="detail_errormsg">
           Sorry! We couldn't find that character in any known universe!
         </p>
-        <Link className='detail_back' to='/'>
-          <i className='fa-solid fa-circle-left'></i> Go back
+        <Link className="detail_back" to="/">
+          <i className="fa-solid fa-circle-left"></i> Go back
         </Link>
       </div>
     );
